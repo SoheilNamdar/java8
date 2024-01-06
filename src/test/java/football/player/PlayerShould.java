860c0d0d0fab18e985c7528186b0d0de8c826a76
SoheilNamdar <soheilnamdar@yahoo.com>
2024-01-06 20:02:34 +0100

    PlayerShould changed

--- src/test/java/football/player/PlayerShould.java
+++ src/test/java/football/player/PlayerShould.java
@@ -4,6 +4,7 @@ import helper.PlayerTestHelper;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,16 +12,17 @@ public class PlayerShould {
     @Test
     void give_the_best_score_with_oop() {
 
-        final List<Player> scorers = new PlayerTestHelper().getPlayers();
+        final List<Player> scorers = new LinkedList<>();
+        scorers.add(new Player("Ali DAEI", 109));
+        scorers.add(new Player("Christian RONALDO", 115));
+        scorers.add(new Player("Mokhtar DAHARI", 89));
 
-        Player bestScore = scorers.get(0);
+        Player bestScorer = scorers.get(0);
         for (Player scorer : scorers) {
-            if(scorer.getGoal() > bestScore.getGoal()){
-                bestScore = scorer;
-            }
+            if(scorer.getGoal() > bestScorer.getGoal())
+                bestScorer = scorer;
         }
-        System.out.println(bestScore.getName());
-        assertThat(bestScore.equals("Christian RONALDO"));
+        System.out.println(bestScorer.getName());
     }
     @Test
     void give_best_score_fp() {
