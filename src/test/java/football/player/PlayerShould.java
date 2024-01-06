d5076758d750de3ce379b5cfe5ac28432089aa1b
SoheilNamdar <soheilnamdar@yahoo.com>
2024-01-06 20:13:35 +0100

    PlayerShould changed

--- src/test/java/football/player/PlayerShould.java
+++ src/test/java/football/player/PlayerShould.java
@@ -1,11 +1,10 @@
 package football.player;
 import football.playes.Player;
 import helper.PlayerTestHelper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 public class PlayerShould {
@@ -22,12 +21,19 @@ public class PlayerShould {
             if(scorer.getGoal() > bestScorer.getGoal())
                 bestScorer = scorer;
         }
-        System.out.println(bestScorer.getName());
+        // System.out.println(bestScorer.getName());
+        Assertions.assertThat(bestScorer.getName()).isEqualTo("Christian RONALDO");
     }
     @Test
     void give_best_score_fp() {
-        List<Player> playerList = new PlayerTestHelper().getPlayers();
-        playerList.stream().max(Comparator.comparing(player -> player.getGoal())).get();
+        List<Player> scorers = new ArrayList<>();
+        scorers.add(new Player("Ali DAEI", 109));
+        scorers.add(new Player("Christian RONALDO", 115));
+        scorers.add(new Player("Mokhtar DAHARI", 89));
+
+        final Player bestScorer = scorers.stream().max(Comparator.comparing(player -> player.getGoal())).get();
+
+        Assertions.assertThat(bestScorer.getName()).isEqualTo("Christian RONALDO");
     }
 
 }
